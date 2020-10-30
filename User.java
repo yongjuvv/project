ed897ae4c805bd394d596f3998930bc1ce6be230
yongjuvv <o01071767279@nate.com>
2020-10-30 20:06:37 +0900

    example

---
+++ User.java
@@ -0,0 +1,41 @@
+package user;
+
+public class User {
+	private String id;
+	private String password;
+	private String userName;
+	private String userGender;
+	private String userEmail;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserGender() {
+		return userGender;
+	}
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
+}
